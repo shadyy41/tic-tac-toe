8dc297b3b818d5cef2fabd783cc6dcfb383429bd
shady41 <abhinavjha2512@gmail.com>
2023-01-27 02:47:40 +0530

    Fixed empty games array

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
       const res = await fetch(`https://tic-tac-toe-backend-mm5k.onrender.com/userdata/${userData.username}`)
       const parsed = await res.json()
       if(!parsed.success) throw new Error()
-      setUserGames(parsed.data.games)
+      setUserGames(parsed.data?.games)
     }catch(e){
       console.log(e)
     }
